eac4382137701ab12a7131200239c28372c6f803
WissamElJ <wissam.eljablaoui@gmail.com>
2024-03-17 00:22:48 +0300

    url changed

--- Thesis.py
+++ Thesis.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 
+url = "https://github.com/WissamElJ/Thesis_pdf/blob/a5ea1b7942d144ebcf33ee349e7fc2c4229b9415/Thesis.pdf"
 
-
-pdf_viewer("Thesis.pdf",
-           rendering='unwrap')
+pdf_viewer(url)
