7bc0514287de5097a76f203f7d42736d00849a87
WissamElJ <wissam.eljablaoui@gmail.com>
2024-03-17 11:03:39 +0300

    final change

--- Thesis.py
+++ Thesis.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
+import base64
 
-url = "https://github.com/WissamElJ/Thesis_pdf/blob/a5ea1b7942d144ebcf33ee349e7fc2c4229b9415/Thesis.pdf"
+    # Convert to utf-8
+with open("Thesis.pdf", "rb") as file:
+    base64_pdf = base64.b64encode(file.read()).decode('utf-8')
 
-pdf_viewer(url)
+
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>' 
+st.markdown(pdf_display, unsafe_allow_html=True)
